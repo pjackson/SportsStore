d4a2703df5fa08de939a5dc0365e3776c4b28b74
Pam <pjackso9@gmail.com>
2016-11-27 23:52:06 -0600

    Chapter 6

--- SportsStore/SportsStore.Domain/Entities/Product.cs
+++ SportsStore/SportsStore.Domain/Entities/Product.cs
@@ -10,9 +10,13 @@ namespace SportsStore.Domain.Entities
     public class Product
     {
         public int ProductID { get; set; }
+
         public string Name { get; set; }
+
         public string Description { get; set; }
+
         public decimal Price { get; set; }
+
         public string Category { get; set; }
     }
 }
--- SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Web.Mvc;
 using Moq;
 using Ninject;
 using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
 using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
@@ -26,16 +26,16 @@ namespace SportsStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
-            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
-            mock.Setup(m => m.Products)
-                .Returns(new List<Product>
-                {
-                    new Product { Name = "Football", Price = 25 },
-                    new Product { Name = "Surf board", Price = 179 },
-                    new Product { Name = "Running shoes", Price = 95 }
-                });
+            //Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            //mock.Setup(m => m.Products)
+            //    .Returns(new List<Product>
+            //    {
+            //        new Product { Name = "Football", Price = 25 },
+            //        new Product { Name = "Surf board", Price = 179 },
+            //        new Product { Name = "Running shoes", Price = 95 }
+            //    });
 
-            kernel.Bind<IProductsRepository>().ToConstant(mock.Object);
+            kernel.Bind<IProductsRepository>().To<EFProductRepository>();
 
         }
     }
